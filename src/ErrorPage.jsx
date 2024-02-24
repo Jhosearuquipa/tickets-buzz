dc872ee461d723f660e0b5f50c5d05d77639e1a1
Jhosearuquipa <jaruquipa@dguiaf-gcba.gov.ar>
2024-02-24 01:19:35 -0300

    add styling to error page and create the file structure

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -4,15 +4,15 @@ import React from 'react'
 export default function ErrorPage() {
 
    const error = useRouteError();
-   console.error(error);
+   //console.error(error);
 
    return (
-      <div id="error-page">
-         <h1>Oops!</h1>
-         <p>Sorry, an unexpected error has occurred.</p>
-         <p>
-            <i>{error.statusText || error.message}</i>
-         </p>
-      </div>
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+         <div className="text-center">
+            <p className="text-base font-semibold text-indigo-600">Oops!</p>
+            <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">{error.statusText || error.message}</h1>
+            <p className="mt-6 text-base leading-7 text-gray-600">Sorry, an unexpected error has occurred.</p>
+         </div>
+      </main>
    );
 }
