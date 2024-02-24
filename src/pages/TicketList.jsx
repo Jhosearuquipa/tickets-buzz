af997ab76793e6a143ee821e994f8fade0a0aa8e
Jhosearuquipa <jaruquipa@dguiaf-gcba.gov.ar>
2024-02-24 02:57:59 -0300

    create ticket list

--- src/pages/TicketList.jsx
+++ src/pages/TicketList.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import TicketItem from '../components/TicketItem'
 
 export default function TicketList() {
    return (
-      <div>
+      <div className="container mx-auto p-8">
          <p>Listado de tickets</p>
+         <ul role="list" class="divide-y divide-gray-100">
+            <TicketItem />
+         </ul>
       </div>
    )
 }
